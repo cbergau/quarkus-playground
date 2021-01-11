e526f69ac517d7bb2ba6a34fd626d950be0913ba
christianbergau <cbergau86@gmail.com>
2021-01-11 17:31:44 +0100

    swagger openapi 3.0

---
+++ .history/src/main/java/de/christianbergau/MainApplication_20210111172736.java
@@ -0,0 +1,8 @@
+package de.christianbergau;
+
+import io.quarkus.runtime.Application;
+
+
+public class MainApplication extends Application {
+
+}
---
+++ .history/src/main/java/de/christianbergau/MainApplication_20210111172907.java
@@ -0,0 +1,24 @@
+package de.christianbergau;
+
+import javax.ws.rs.core.Application;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title="Example API",
+        version = "1.0.1",
+        contact = @Contact(
+            name = "Example API Support",
+            url = "http://exampleurl.com/contact",
+            email = "techsupport@example.com"),
+        license = @License(
+            name = "Apache 2.0",
+            url = "http://www.apache.org/licenses/LICENSE-2.0.html"))
+)
+public class MainApplication extends Application {
+
+}
